0081c67370f568ae4eceaf520b700c5bb3c465fe
SydannyOpal <sydanny15@gmail.com>
2022-05-11 21:03:18 -0400

    implementing constants module

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const net = require("net");
-
+const { IP, PORT } = require("./constants");
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: '165.227.47.243', // IP address here,
     port: 50541, // PORT number here,
@@ -14,13 +14,14 @@ const connect = function () {
 
     console.log("connected user says:", data);
 
-  })
+  });
 
   conn.on('connect', function() {
-    console.log('connected')
-    conn.write('Name: SYD')
-    conn.write('Say: snaakelit')
-  return conn;
-});
+    console.log('connected');
+    conn.write('Name: SYD');
+    conn.write('Say: snaakelit');
+    return conn;
+  });
 
+};
 module.exports = {connect};
---
+++ constants.js
@@ -0,0 +1,9 @@
+const conn = net.createConnection({
+  IP: '165.227.47.243', 
+  PORT: 50541,
+});
+
+module.exports = {
+  IP,
+  PORT,
+}; 
